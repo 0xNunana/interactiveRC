e930c99993a129b2da831f08f820cb51c03ee040
0xnunana <kudayapaul@gmail.com>
2023-03-15 22:17:10 +0000

    input type changed

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,13 @@
 import React from 'react'
 import {StarIcon} from '@heroicons/react/24/outline'
+import {Link} from "react-router-dom"
+// import { useState } from 'react'
 
 const Home = () => {
+    // const [clicked,setClicked]=useState();
+    // const handleClick=()=>{
+    //     setClicked(e.target.value)
+    // };
   return (
     <div className="bg-gray-900 items-center flex justify-center py-10" >
       <div className="bg-gray-800 rounded-3xl my-10 h-50 p-7 text-white">
@@ -13,13 +19,17 @@ const Home = () => {
           Please let us know how we did with your support <br/>request.All feedback is appreciated to help us<br/> improve our offering!
         </div>
         <div className='space-x-7 py-4 text-gray-400 '>
-          <button variant="contained" className='hover:bg-slate-300 rounded-full w-[50px] h-[50px] bg-gray-700' type=''>1</button>
-          <button className='hover:bg-slate-300 rounded-full w-[50px] h-[50px] bg-gray-700'>2</button>
-          <button className='hover:bg-slate-300 rounded-full w-[50px] h-[50px] bg-gray-700'>3</button>
-          <button className='hover:bg-slate-300 rounded-full w-[50px] h-[50px] bg-gray-700'>4</button>
-          <button className='hover:bg-slate-300 rounded-full w-[50px] h-[50px] bg-gray-700'>5</button>
+          <input type="button"  className='hover:bg-slate-300 rounded-full w-[50px] h-[50px] bg-gray-700' value="1"/>
+          <input type="button" className='hover:bg-slate-300 rounded-full w-[50px] h-[50px] bg-gray-700' value="2"/>
+          <input type="button" className='hover:bg-slate-300 rounded-full w-[50px] h-[50px] bg-gray-700' value="3"/>
+          <input type="button" className='hover:bg-slate-300 rounded-full w-[50px] h-[50px] bg-gray-700' value="4"/>
+          <input type="button" className='hover:bg-slate-300 rounded-full w-[50px] h-[50px] bg-gray-700' value="5"/>
         </div>
-        <div className='w-full my-3 '><button className='bg-orange-500 w-full rounded-3xl h-15 py-2 font-semibold' >SUBMIT</button>
+        <div className='w-full my-3 '>
+            <Link to="/success">
+            <button className='bg-orange-500 w-full rounded-3xl h-15 py-2 font-semibold' >SUBMIT</button>
+            </Link>
+            
       </div>
     </div>
     </div>
