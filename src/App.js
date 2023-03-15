8e0545fcd88dd01d7727b4e0e5f1716d2dacc5b7
0xnunana <kudayapaul@gmail.com>
2023-03-15 13:54:28 +0000

    outlook and logos

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-black/90 items-center flex justify-center" >
+      <div className="bg-gray-700 rounded my-5 h-50 p-7 text-white">
+        <div>star</div>
+        <div>How did we do?</div>
+        <div>
+          Please let us know how we did with your support request.All feedback is appreciated to help us improve our offering!
+        </div>
+        <div>
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>4</button>
+          <button>5</button>
+        </div>
+        <div><button>Submit</button></div>
+      </div>
     </div>
   );
 }
